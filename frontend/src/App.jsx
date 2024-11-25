345d1e2a78790f1c0ec90c91bf121c6ca3e4ab75
Michael Wilke <mwilke@enarchtech.com>
2024-11-25 16:55:51 -0600

    Setup Routes in App.js and removed old code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import DropdownMenu from './components/Dropdown';
-import DiscordButton from './components/Discordbutton';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './home';
+import Dashboard from './dashboard'
 
 function App() {
   return (
-    <div style = {{padding: "50px"}}>
-      <DiscordButton></DiscordButton>
-        <br />
-        <br />
-      <DropdownMenu></DropdownMenu>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   )
 }
 
