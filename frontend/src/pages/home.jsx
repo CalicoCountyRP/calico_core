bbce22d871613555655e70272388073067a407fa
Michael Wilke <mwilke@enarchtech.com>
2024-12-04 09:15:30 -0600

    home page styles

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -7,10 +7,14 @@ import '../App.css'; // Make sure to import the CSS file
 const Home = () => {
     return( 
         <div className="home-container">
-            <img src={logo} width={250} height={250} alt="Logo" />
-            <h1>Calico County Core</h1>
-            <p>Please Login with Discord</p>
-            <DiscordButton />
+            <div className="login">
+                <br />
+                <img src={logo} width={250} height={250} alt="Logo" />
+                <h1>Calico County Core</h1>
+                <br />
+                <br />
+                <DiscordButton />
+            </div>
         </div>
     )
 };
