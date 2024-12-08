fcb1b990ea1b77c24bcdd06ff25fdf8aa67842f4
Michael Wilke <mwilke@enarchtech.com>
2024-12-07 22:13:32 -0600

    Added Logout Icon to Top Nav Bar

--- frontend/src/components/topbar.jsx
+++ frontend/src/components/topbar.jsx
@@ -5,10 +5,11 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import logo from '../assets/cclogo.png';
+import { MdLogout } from "react-icons/md";
 
 const handleLogout = () => {
     localStorage.clear();
-    
+
     // Add your logout logic here
     console.log('Logout button clicked');
 };
@@ -18,11 +19,16 @@ const TopBar = () => {
         <AppBar position="static" sx={{ backgroundColor: '#333' }}>
             <Toolbar>
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1, display: 'flex', alignItems: 'center' }}>
-                    <img src={logo} alt="Calico County Logo" style={{ width: '40px', height: '40px' }} />
+                    <img src={logo} alt="Calico County Logo" style={{ width: '40px', height: '40px', marginRight: '10px' }} />
                     Calico County Core
                 </Typography>
                 <Stack direction="row" spacing={2}>
-                    <Button color="inherit" onClick={handleLogout} href='http://localhost:8081/logout'>
+                    <Button 
+                        color="inherit" 
+                        onClick={handleLogout} 
+                        href='http://localhost:8081/logout'
+                        startIcon={<MdLogout />}
+                    >
                         Logout
                     </Button>
                 </Stack>
