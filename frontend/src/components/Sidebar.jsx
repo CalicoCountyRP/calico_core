3d4221e0abf5353e2273c6451beeeae69c950dad
Michael Wilke <mwilke@enarchtech.com>
2024-12-07 22:10:46 -0600

    Added Icons to the Sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 import { Link } from 'react-router-dom';
+import { MdOutlineSpaceDashboard, MdOutlineRealEstateAgent } from "react-icons/md";
+import { BsPeople } from "react-icons/bs";
+import { GiStable, GiScrollQuill } from "react-icons/gi";
+import { LuScrollText } from "react-icons/lu";
+
 import '../App.css';
 
 function SidebarNav() {
@@ -8,15 +13,23 @@ function SidebarNav() {
         <div className='sidenav'>
             <Sidebar>
                 <Menu className="menu">
-                    <MenuItem component={<Link to="/pages/dashboard" />}>Dashboard</MenuItem>
-                    <MenuItem component={<Link to="/pages/character" />}>Characters</MenuItem>
-                    <MenuItem component={<Link to="/pages/stable" />}>Stable</MenuItem>
-                    <SubMenu label="Real Estate">
+                    <MenuItem icon={<MdOutlineSpaceDashboard />} component={<Link to="/pages/dashboard" />}>
+                        Dashboard
+                    </MenuItem>
+                    <MenuItem icon={<BsPeople />} component={<Link to="/pages/character" />}>
+                        Characters
+                    </MenuItem>
+                    <MenuItem icon={<GiStable />} component={<Link to="/pages/stable" />}>
+                        Stable
+                    </MenuItem>
+                    <SubMenu label="Real Estate" icon={<MdOutlineRealEstateAgent />}>
                         <MenuItem component={<Link to="/pages/businesses" />}>Businesses</MenuItem>
                         <MenuItem component={<Link to="/pages/properties" />}>Properties</MenuItem>
                     </SubMenu>
-                    <MenuItem component={<Link to="/pages/government" />}>Government</MenuItem>
-                    <SubMenu label="Applications">
+                    <MenuItem icon={<LuScrollText />} component={<Link to="/pages/government" />}>
+                        Government
+                    </MenuItem>
+                    <SubMenu label="Applications" icon={<GiScrollQuill />}>
                         <MenuItem component={<Link to="/pages/applications/staff" />}>Staff Application</MenuItem>
                         <MenuItem component={<Link to="/pages/applications/developer" />}>Developer Application</MenuItem>
                         <MenuItem component={<Link to="/pages/applications/tester" />}>Dev Tester Application</MenuItem>
