614892bd95476255c8db724fe470d6bc068387f2
Michael Wilke <mwilke@enarchtech.com>
2024-12-07 21:50:17 -0600

    Added Discord Icon to login Button

--- frontend/src/components/Discordbutton.jsx
+++ frontend/src/components/Discordbutton.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import Button from '@mui/material/Button';
-
+import { FaDiscord } from 'react-icons/fa';
 
 function DiscordButton() {
     return (
-    <div>
-        <Button className="discordButton" variant="contained" href='http://localhost:8081/auth/discord/login'> Login With Discord </Button>
-    </div>
-    )
+        <div>
+            <Button
+                className="discordButton"
+                variant="contained"
+                href='http://localhost:8081/auth/discord/login'
+                startIcon={<FaDiscord />}
+            >
+                Login With Discord
+            </Button>
+        </div>
+    );
 }
 
 export default DiscordButton;
