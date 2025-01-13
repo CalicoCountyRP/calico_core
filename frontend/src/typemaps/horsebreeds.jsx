c146cdb3637f9a68f81a44a632c77cb86e594f15
Michael Wilke <mwilke@enarchtech.com>
2025-01-12 23:14:58 -0600

    Update horsebreeds.jsx

--- frontend/src/typemaps/horsebreeds.jsx
+++ frontend/src/typemaps/horsebreeds.jsx
@@ -1,4 +1,23 @@
 const BreedsTypeMap = {
     'a_c_horse_arabian_black': 'Arabian Black',
+    'a_c_horse_americanpaint_greyovero': 'American Paint',
+    'a_c_horse_americanpaint_overo': 'American Paint',
+    'a_c_horse_americanpaint_splashedwhite': 'American Paint', 
+    'a_c_horse_americanpaint_tobiano': 'American Paint',
+    'a_c_horse_americanstandardbred_black': 'American Standardbred',
+    'a_c_horse_americanstandardbred_buckskin': 'American Standardbred',
+    'a_c_horse_americanstandardbred_lightbuckskin': 'American Standardbred',
+    'a_c_horse_americanstandardbred_palominodapple': 'American Standardbred',
+    'a_c_horse_americanstandardbred_silvertailbuckskin': 'American Standardbred',
+    'a_c_horse_andalusian_darkbay': 'Andalusian',
+    'a_c_horse_andalusian_perlino': 'Andalusian',
+    'a_c_horse_andalusian_rosegray': 'Andalusian',
+    'a_c_horse_appaloosa_blacksnowflake': 'Appaloosa',
+    'a_c_horse_appaloosa_blanket': 'Appaloosa',
+    'a_c_horse_appaloosa_brownleopard': 'Appaloosa',
+    'a_c_horse_appaloosa_fewspotted_pc' 'Appaloosa',
+    'a_c_horse_appaloosa_leopard' 'Appaloosa',
+    'a_c_horse_appaloosa_leopardblanket' 'Appaloosa',
+    
 };
 export default BreedsTypeMap;
